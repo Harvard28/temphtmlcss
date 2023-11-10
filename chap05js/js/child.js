58c7e67c3807a97985af98d17bb62f380ebac4dc
Harvard28 <144681667+Harvard28@users.noreply.github.com>
2023-11-10 20:32:31 +0700

    chap05js

---
+++ chap05js/js/child.js
@@ -0,0 +1,5 @@
+var strartItem = document.getElementsByTagName('ul')[0];
+var firstIem = strartItem.firstChild;
+var lastItem = strartItem.lastChild;
+firstIem.className = 'complete';
+lastItem.className = 'cool';
---
+++ chap05js/js/sibling.js
@@ -0,0 +1,6 @@
+var startIem = document.getElementById('two');
+var prevItem = startIem.previousSibling;
+var nextItem = startIem.nextSibling;
+
+prevItem.className = 'complete';
+nextItem.className = 'cool';
